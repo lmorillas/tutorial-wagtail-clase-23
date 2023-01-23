b2457d4b58d42ec236dd1dc9b0c6588d40ec9a18
Luis Miguel <morillas@gmail.com>
2023-01-23 13:37:27 +0100

    tags en blog

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,8 +1,14 @@
 from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel, InlinePanel
+from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.search import index
 from django.db import models
+
+# Nuevos imports para ClusterTaggableManager, TaggedItemBase, MultiFieldPanel
+
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
 
 from modelcluster.fields import ParentalKey
 
@@ -20,10 +26,18 @@ class BlogIndexPage(Page):
         context['paginas'] = blogpages
         return context
 
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
 
     def main_image(self):
         'seleciona a primera imagem de la galeria'
@@ -39,7 +53,10 @@ class BlogPage(Page):
     ]
 
     content_panels = Page.content_panels + [
-        FieldPanel('date'),
+        MultiFieldPanel([
+            FieldPanel('date'),
+            FieldPanel('tags'),
+        ], heading="Blog information"), # Heading es el titulo del panel
         FieldPanel('intro'),
         FieldPanel('body'),
         InlinePanel('gallery_images', label="Gallery images"),
@@ -57,3 +74,21 @@ class BlogPageGalleryImage(Orderable):
         FieldPanel('image'),
         FieldPanel('caption'),
     ]
+
+class BlogTagIndexPage(Page):
+    '''
+    Pagina que muestra todas las etiquetas.
+    Recuerda que tienes que crear una pagina de este tipo en el panel de administracion 
+    con el slug "tags"
+    '''
+
+    def get_context(self, request):
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context['blogpages'] = blogpages
+        return context

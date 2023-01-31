c8051e4c9b3132848bbb69fd6fc7c5bf70198076
Luis Miguel <morillas@gmail.com>
2023-01-31 21:53:42 +0100

    postgres

--- README.md
+++ README.md
@@ -7,11 +7,27 @@
 - [X] Instalar debug toolbar
 - [X] Crear app blog
 - [X] Blog Index y Posts
-- [ ] Imágenes y documentos
-- [ ] Tags y categorías
-- [ ] Carrusel en home (Buscar librería, por ejemplo https://glidejs.com/)
+- [X] Imágenes y documentos
+- [X] Tags y categorías
+- [X] Carrusel en home (Buscar librería, por ejemplo https://glidejs.com/)
 - [ ] Búsqueda https://docs.wagtail.org/en/stable/topics/search/index.html
 - [ ] Menús https://wagtailmenus.readthedocs.io/en/stable/
 - [ ] Sección de noticias en primera página
 - [ ] Paginar noticias y entradas de blog
 - [ ] Página de conacto
+- [ ] Migrar a postgresql [Vagrant](https://github.com/lmorillas/vagrant_docker/tree/postgres)
+- [ ] Backups de la base de datoshttps://django-dbbackup.readthedocs.io
+
+
+## Backups
+
+```bash
+pg_dump -Fc miproyecto -U postgres | gzip >/backups/example_database-$(date +%Y-%m-%d).dump.gz
+
+pg_restore -d miproyecto -U postgres -v /backups/example_database-2019-01-01.dump.gz
+```
+
+```bash
+docker-compose exec db bash -c 'pg_dump -Fc miproyecto -U postgres | gzip >/backups/example_database-$(date +%Y-%m-%d).dump.gz'
+
+```
--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+DB = os.getenv("DB")
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
@@ -89,13 +96,24 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+if DB == "sqlite":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
-
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": POSTGRES_DB,
+            "USER": POSTGRES_USER,
+            "PASSWORD": POSTGRES_PASSWORD,
+            "HOST": "localhost",
+            "PORT": 5432,
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -119,7 +137,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "es-es"
+LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "Europe/Madrid"
 

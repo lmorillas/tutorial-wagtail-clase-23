3f4210620533cb174b4b558d031be14e54f3af7a
Luis Miguel <morillas@gmail.com>
2023-02-01 18:32:31 +0100

    doc de fabfile

--- mysite/fabfile.py
+++ mysite/fabfile.py
@@ -1,5 +1,7 @@
 from fabric import task, Connection
 from invocations.console import confirm
+from dotenv import load_dotenv
+import os
 
 '''
 Fabfile con ejemplos de gestión del proyecto
@@ -7,29 +9,38 @@ Fabfile con ejemplos de gestión del proyecto
 Recuerda instalar fabric e invocations
 $ pip install fabric invocations
 '''
+load_dotenv()
+POSTGRES_DB = os.getenv('POSTGRES_DB')
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+
 
 my_hosts = []
 
 @task
 def run(c):
+    '''Ejecuta el servidor de desarrollo'''
     c.run('python manage.py runserver', pty=True)
 
 @task
 def vagup(c):
+    '''Inicia la máquina virtual con provisionamiento'''
     c.run('vagrant up --provision', pty=True)
 
 @task
 def backup(c):
-    c.run('''vagrant ssh -c "cd /vagrant && \
-        docker-compose exec db bash -c 'pg_dump -Fc miproyecto -U postgres | gzip >/backups/example_database-$(date +%Y-%m-%d).dump.gz'"''',
+    '''Crea un backup de la base de datos'''
+    c.run(f'''vagrant ssh -c "cd /vagrant && \
+        docker-compose exec db bash -c 'pg_dump -Fc {POSTGRES_DB} -U {POSTGRES_USER} | gzip >/backup/{POSTGRES_DB}-$(date +%Y-%m-%d).dump.gz'"''',
         pty=True)
 
 def conexion_vagrant():
+    '''Devuelve una conexión a la máquina virtual'''
     return Connection('localhost', user='vagrant', port=2222,
         connect_kwargs={"password": "vagrant"})
     
 @task
 def dockerls(ctx):
+    '''Lista los contenedores'''
     c = conexion_vagrant()
 
     c.run('docker ps -a', pty=True)
@@ -37,11 +48,13 @@ def dockerls(ctx):
 
 @task
 def dockerlogs(ctx):
+    '''Muestra los logs del contenedor de la base de datos'''
     c = conexion_vagrant()
     c.run('cd /vagrant && docker-compose logs db', pty=True)
 
 @task
 def gitpush(ctx):
+    '''Hace un commit y un push'''
     ctx.run('git status')
     question = '¿Estás seguro?'
     respuesta = confirm(question, assume_yes=True)
